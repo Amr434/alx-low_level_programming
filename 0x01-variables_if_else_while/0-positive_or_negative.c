59395ebaab22c34aaf1087f5f73f473ea81eaa42
Amr434 <amrkasban37@gmail.com>
2023-02-17 11:19:35 -0800

    negative postive

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include<time.h>
+int main(void)
+{
+srand(time(NULL));
+int num = rand();
+
+if (num > 0)
+printf("%d is positive\n", num);
+else if (num == 0)
+{
+printf("%d is zero\n", num);
+}
+else
+{
+printf("%d is negative\n", num);
+}
+return (0);
+}
