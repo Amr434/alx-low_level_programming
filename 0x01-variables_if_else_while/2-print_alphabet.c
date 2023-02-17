4ccea20a2b77dc0ae81b194facbc149238c0b2fd
Amr434 <amrkasban37@gmail.com>
2023-02-17 13:11:40 -0800

    print alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch;
+
+ch = 'a';
+while (ch != ('z' + 1))
+{
+putchar(ch);
+ch += 1;
+}
+
+putchar('\n');
+
+return (0);
+}
