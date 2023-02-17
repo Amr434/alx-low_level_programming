290bd0fcff75f39941dd001c9404bf4afd075ef3
Amr434 <amrkasban37@gmail.com>
2023-02-17 14:48:07 -0800

    6print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,17 +6,14 @@
 */
 int main(void)
 {
+int num;
 
-putchar('0');
-putchar('1');
-putchar('2');
-putchar('3');
-putchar('4');
-putchar('5');
-putchar('6');
-putchar('7');
-putchar('8');
-putchar('9');
+for (num = 0 ; num < 10 ; num++)
+{
+
+putchar('0' +  num);
+
+}
 putchar('\n');
 return (0);
 }
