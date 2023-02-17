7469cc003734d4b7031a5650fc1e549013d97201
Amr434 <amrkasban37@gmail.com>
2023-02-17 13:53:46 -0800

    added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void)
+{
+char ch;
+ch = 'a';
+
+for (ch = 'a' ; ch < 'z' ; ch++)
+{
+
+if (ch == ('p' + 1) || ch == ('d' + 1))
+{
+continue;
+}
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
+
