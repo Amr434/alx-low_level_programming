0d01a3755e0df171ef20b25a67b92a4d5c827fce
Amr kasban <96775757+Amr434@users.noreply.github.com>
2023-02-17 21:53:00 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,11 +16,11 @@ srand(time(0));
 
 if (num > 0)
 printf("%d is positive\n", num);
-else if (num < 0)
+if (num < 0)
 {
 printf("%d is negative\n", num);
 }
-else
+if( (num == 0)
 {
 printf("%d is zero\n", num);
 }
