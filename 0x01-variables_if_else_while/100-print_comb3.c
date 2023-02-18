c8b4b96590461d5a4ff3fee72e7a5e12ca0b2e73
Amr434 <amrkasban37@gmail.com>
2023-02-18 01:36:29 -0800

    100-print,comm

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(void)
+{
+
+int x, i;
+for (x = 0 ; x < 10 ; x++)
+{
+for (i = x + 1 ; i < 10 ; i++)
+{
+
+putchar('0' + x);
+putchar('0' + i);
+
+if (x == 8 && i == 9)
+{
+continue;
+}
+
+putchar(',');
+putchar(' ');
+
+}
+}
+putchar('\n');
+return (0);
+
+}
