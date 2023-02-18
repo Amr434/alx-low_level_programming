ce534b0dfb986661c812c9cb948275b7c11dc84f
Amr434 <amrkasban37@gmail.com>
2023-02-18 01:00:12 -0800

    print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - is entry of program
+ *
+ * Return: 0, if succesful
+ */
+int main(void)
+{
+int x;
+
+for (x = 0 ; x < 10 ; x++)
+{
+putchar((x % 10) + '0');
+
+if (x == 9)
+/*continue printing comma and space except if x == 9*/
+continue;
+
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
