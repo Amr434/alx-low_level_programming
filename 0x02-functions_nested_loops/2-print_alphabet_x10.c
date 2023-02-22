316659750a041f497e55df934e815b6bfc54b424
Amr434 <amrkasban37@gmail.com>
2023-02-22 01:02:57 -0800

    10print

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "main.h"
+void print_alphabet_x10()
+{
+int i;
+char ch;
+
+for (i = 0 ; i < 10 ; i++)
+{
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+
+_putchar(ch);
+
+}
+
+_putchar('\n');
+}
+}
