fe9aa69fb36c8575d223166013b38fcb57c5e507
Amr kasban <96775757+Amr434@users.noreply.github.com>
2023-02-22 17:40:19 +0200

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,10 +2,11 @@
 #include <ctype.h>
 
 /**
-*_isalpha-check if input is alpha or not
-*@c is  input  user enter it
-*_isalph return 1 if isalpha 0 if not
-*/
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
+ */
 int _isalpha(int c)
 {
 if (isalpha(c) > 0)
