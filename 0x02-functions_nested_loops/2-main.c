0408c7ec081e37aaa9151f6f38ad5f991cbd9388
Amr434 <amrkasban37@gmail.com>
2023-02-22 01:10:42 -0800

    add comment

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -7,6 +7,9 @@
  */
 int main(void)
 {
+/**
+ * print alphabet from a-z 10 time
+*/
 print_alphabet_x10();
 return (0);
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include "main.h"
-/*
+/**
  * this func print alpha from a-z 10 time
 */
 void print_alphabet_x10(void)
