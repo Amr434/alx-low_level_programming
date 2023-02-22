9b1db9c30f936699559d873fc7750f93d9328e49
Amr434 <amrkasban37@gmail.com>
2023-02-22 07:01:59 -0800

    is lower func

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/*_islower-function check if is lower or not*/
+int _islower(int c)
+{
+if (islower(c) > 0)
+{
+return (1);
+}
+else
+
+{
+return (0);
+}
+}
