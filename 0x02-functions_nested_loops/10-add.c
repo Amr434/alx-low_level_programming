8898e8115e930df0f83b88ae3ff72b1b3bb0fcd0
Amr kasban <96775757+Amr434@users.noreply.github.com>
2023-02-23 07:36:43 +0200

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
 *add-function sum two number
-*@num1,@num2: two paramters checked
+*@num1: paramter checked
+*@num2: paramter checked
 *
-*Return function return the sumation of two paramters
+*Return: function return the sumation of two paramters
 */
 int add(int num1, int num2)
 {
