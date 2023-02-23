4a823ff6557821b7a815df7716ce9d950e4c2905
Amr434 <amrkasban37@gmail.com>
2023-02-22 21:18:10 -0800

    add two number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*add-function sum two number
+*@num1,@num2: two paramters checked
+*
+*Return function return the sumation of two paramters
+*/
+int add(int num1, int num2)
+{
+
+return (num1 + num2);
+}
