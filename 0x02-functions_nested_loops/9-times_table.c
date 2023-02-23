2daad946a916b4cf73b5691361324f5fb72e7b02
Amr kasban <96775757+Amr434@users.noreply.github.com>
2023-02-23 06:32:34 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,21 +10,27 @@ int i, j;
 for (i = 0 ; i <= 9 ; i++)
 {
 for (j = 0 ; j <= 9 ; j++)
-{
+{ 
 if ((i * j) >= 10)
 {
 _putchar(((i * j) / 10) + '0');
 _putchar(((i * j) % 10) + '0');
+if (j == 9)
+continue;
+_putchar(',');
+_putchar(' ');
 }
 else
 {
 _putchar((i * j) + '0');
-}
 if (j == 9)
 continue;
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
+}
+
+
 }
 _putchar('\n');
 }
