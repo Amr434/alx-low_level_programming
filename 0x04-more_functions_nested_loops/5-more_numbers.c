d5e261a98745060fc7499a2fb93eb638c2b9d583
Amr434 <amrkasban37@gmail.com>
2023-02-24 15:06:34 -0800

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*more_numbers - print from 0-14 9-times
+*/
+void more_numbers(void)
+{
+int i, j;
+
+for (i = 0 ; i <= 9 ; i++)
+{
+for (j = 0 ; j <= 14 ; j++)
+{
+
+if (j > 9)
+{
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+}
+else
+{
+_putchar(j + '0');
+}
+}
+_putchar('\n');
+}
+}
