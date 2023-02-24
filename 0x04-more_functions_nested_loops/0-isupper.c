4e334593c4711cce9d6cc0ae0d3a428fc496d6ae
Amr434 <amrkasban37@gmail.com>
2023-02-24 13:01:54 -0800

    is upper func

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <ctype.h>
 /**
 *isupper-function check if charachter is upper or is no
 *
@@ -9,7 +8,7 @@
 */
 int _isupper(int c)
 {
-if (isupper(c))
+if (c >= 'A' && c <= 'Z')
 {
 return (1);
 }
