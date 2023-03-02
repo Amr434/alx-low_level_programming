d3cd7d821796700c232cd86051634b98d57962a9
Amr434 <amrkasban37@gmail.com>
2023-03-02 13:21:13 -0800

    and if check

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,7 +18,10 @@ if (str[i] == '\n' || str[i] == ' ' || str[i] == '.')
 str[i + 1] = toupper(str[i + 1]);
 
 if (str[i] == '\t')
+{
 str[i] = ' ';
+str[i + 1] = toupper(str[i + 1]);
+}
 str[i] = str[i];
 }
 return (str);
