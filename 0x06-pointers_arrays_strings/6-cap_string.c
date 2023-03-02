be0a92ba2aeeefea9accd3c520862d75342965d0
Amr434 <amrkasban37@gmail.com>
2023-03-02 14:47:27 -0800

    add if

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,11 +11,10 @@ char *cap_string(char *str)
 {
 int length, i;
 length = strlen(str);
-//char sep[] = " \t\n,;.!?\"(){}";
 
 for (i = 0 ; i < length ; i++)
 {
-if (str[i] == '\n' ||  str[i] == ' '  || str[i] == '.' || str[i] == ',' || str[i]== '!'  || str[i] == '?' || str[i] == '\"' || str[i] == '(' || str[i] == ')' || str[i] =='}' || str[i]== '{')
+if (str[i] == '\n' ||  str[i] == ' '  || str[i] == '.' || str[i] == ',' || str[i] == '!' || str[i] == '?' || str[i] == '\"' || str[i] == '(' || str[i] == ')' || str[i] == '}' || str[i] == '{')
 str[i + 1] = toupper(str[i + 1]);
 
 if (str[i] == '\t')
