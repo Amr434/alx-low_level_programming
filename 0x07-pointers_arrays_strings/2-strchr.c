11803d5ad9e1b5a35ffbc882c4ec14848e78b2f5
Amr kasban <96775757+Amr434@users.noreply.github.com>
2023-03-08 03:38:29 +0200

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,5 +11,18 @@
 char *_strchr(char *s, char c)
 {
 
-return (strchr(s, c));
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+
+	return (NULL);
+}
 }
