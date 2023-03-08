666cb05fa6dddb090032ebfc693150606d38d7db
Amr434 <amrkasban37@gmail.com>
2023-03-07 17:30:26 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+*_strchr a predefined function used for finding the occurence
+*
+*@s: array input.
+*
+*@c: charchter search
+*return: slice of arr
+*/
+char *_strchr(char *s, char c)
+{
+
+return (strchr(s, c));
+}
