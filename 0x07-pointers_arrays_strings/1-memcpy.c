c15ccdfb6a8771e2bc9715210ef37801870d82de
Amr434 <amrkasban37@gmail.com>
2023-03-07 16:21:09 -0800

    copy block

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+*_memcpy - is used to copy a block 
+*
+*@dest:  array of destination.
+*@src:  array of source
+*@n: number of byte.
+*Return: array of byte
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+memcpy(dest, src, n);
+return (dest);
+}
