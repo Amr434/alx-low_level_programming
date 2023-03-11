57bd0dc08238ae3209cc71dbf54a47846a2d86c1
Amr434 <amrkasban37@gmail.com>
2023-03-11 07:40:42 -0800

    add description

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
 #include "main.h"
 #include <stdio.h>
-int main(__attribute__((unused)) int arcg, char * argv [])
+/**
+*main - main Entry point of program
+*@arcg: number of argument in argv.
+*@argv: array of string.
+*return: main return 0 mean project success.
+*/
+int main(__attribute__((unused)) int arcg, char *argv[])
 {
-if( arcg >0)
-printf("%s\n",argv[0]);
+if (arcg > 0)
+printf("%s\n", argv[0]);
+
 return (0);
 }
