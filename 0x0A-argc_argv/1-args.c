9f52ec4856a759a0c1e6e6fa9c46ec3dc1e7643f
Amr434 <amrkasban37@gmail.com>
2023-03-11 07:51:39 -0800

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+*main - main Entry point of program
+*@arcg: number of argument in argv.
+*@argv: array of string.
+*Return: main return 0 mean project success.
+*/
+int main(int arcg, __attribute__((__unused__)) char *argv[])
+{
+printf("%d\n", arcg);
+}
