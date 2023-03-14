e0feb0a4ca1efde1630676edea679f6eab9bdd96
Amr434 <amrkasban37@gmail.com>
2023-03-14 05:19:53 -0700

    _stardup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include  <stdlib.h>
+#include  <string.h>
+/**
+*_strdup - copy array into another
+*@str: array of char as input
+*Return: copyarray of charachter.
+*/
+char *_strdup(char *str)
+{
+unsigned int i, j;
+char *coparr;
+
+if (str ==  NULL)
+{
+return (NULL);
+}
+for (j = 0 ; str[j] ; j++)
+;
+j++;
+
+coparr = malloc(j *sizeof(char));
+
+if (coparr == NULL)
+return (NULL);
+
+for (i = 0 ; i < j ; i++)
+{
+coparr[i] = str[i];
+}
+
+return (coparr);
+}
