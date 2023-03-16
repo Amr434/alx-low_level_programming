82b87009b0047957b65e52f6da99157d057d6031
Amr kasban <96775757+Amr434@users.noreply.github.com>
2023-03-16 15:40:06 +0200

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,8 +1,7 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <limits.h>
+
 /**
 *malloc_checked -  function that allocates memory using malloc.
 *@b: Number of byte we want.
