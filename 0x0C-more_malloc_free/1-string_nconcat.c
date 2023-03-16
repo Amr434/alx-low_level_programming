579c9099fe6e03ccc276b7d1507f2aad5f20fa78
Amr kasban <96775757+Amr434@users.noreply.github.com>
2023-03-16 17:34:50 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,60 +1,52 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
+
 /**
-*string_nconcat - string with n bytes from another.
-*@s1: first array.
-*@s2: second array.
-*@n: num of bytes;
-*/
+ * string_nconcat - concatenates two strings with a given number of bytes
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes of s2 to copy into s1
+ *
+ * Return: Null if error or char pointer to the new joined place in memory
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i;
-unsigned int x;
-unsigned int j;
-unsigned int f;
-char *newarr;
-i = 0;
-f = 0;
+	unsigned int a;
+	unsigned int b;
+	unsigned int c;
+	char *x;
 
-if (s1 == NULL)
-i=0;
-else 
-{
-while (s1[i])
-i++;
-}
+	if (s1 == NULL)
+		a = 0;
+	else
+	{
+		for (a = 0; s1[a]; a++)
+			;
+	}
 
-if (s2 == NULL)
-f = 0;
-else
-{
-while (s2[f])
-f++;
-}
+	if (s2 == NULL)
+		b = 0;
+	else
+	{
+		for (b = 0; s2[b]; b++)
+			;
+	}
 
-if (n >= f)
-n = f;
+	if (b > n)
+		b = n;
+	x = malloc(sizeof(char) * (a + b + 1));
 
-newarr = malloc(sizeof(char) * (n + i + 1));
+	if (x == NULL)
+		return (NULL);
 
-if (newarr == NULL)
-{
-return (NULL);
-}
+	for (c = 0; c < a; c++)
+		x[c] = s1[c];
 
-for (j = 0 ; s1[j] ; j++)
-{
-newarr[j] = s1[j];
-}
+	for (c = 0; c < b; c++)
+		x[c + a] = s2[c];
 
-for (x = 0 ; x < n ; x++)
-{
-newarr[j] = s2[x];
-j++;
-}
-j++;
-newarr[j] = '\0';
+	x[a + b] = '\0';
 
-
-return (newarr);
+	return (x);
 }
