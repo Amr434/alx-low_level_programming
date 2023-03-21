bbace81e437e7764cfb2ef79bf46eef1dadc6184
Amr434 <amrkasban37@gmail.com>
2023-03-21 16:47:20 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef GRANDPARENT_H
+#define GRANDPARENT_H
+/**
+* struct dog - has specific attribute.
+*@name: dog name
+*@age: dog age.
+*@owner: owner of dog
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif /* GRANDPARENT_H */
